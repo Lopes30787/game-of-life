62828158af574a9e05c77647b85af5d16809d1c4
Lopes30787 <afonsomlopes2001@gmail.com>
2024-01-25 10:53:34 +0100

    Bug Fix

--- GOL.c
+++ GOL.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#include <grower.h>
-#include <glider.h>
-#include <beehive.h>
+#include "grower.h"
+#include "glider.h"
+#include "beehive.h"
 
-#define NUM_ITERATIONS (5000)
-#define SIZE (3000)
+#define NUM_ITERATIONS 5000
+#define SIZE 3000
 
 int GOL[SIZE][SIZE] = {{0}};
 
@@ -14,8 +14,8 @@ int GOL_copy[SIZE][SIZE];
 //Initialize the current pattern.
 //TODO: Fazer esta merda automatica para nao ter de alterar os nomes das variaveis quando quero testar um pattern diferente.
 void initialize() {
-    for(int i = 0; i++; i < GROWER_HEIGHT) {
-        for(int j = 0; j++; j < GROWER_WIDTH) {
+    for(int i = 0; i < GROWER_HEIGHT; i++) {
+        for(int j = 0; j < GROWER_WIDTH; j++) {
             GOL[i+1500][j+1500] = grower[i][j];
         }
     }
@@ -23,8 +23,8 @@ void initialize() {
 
 //Create copy of gol for neighbour counting purposes.
 void copy_gol() {
-    for(int i = 0; i++; i < SIZE) {
-        for(int j = 0; j++; j < SIZE) {
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
             GOL_copy[i][j] = GOL[i][j];
         }
     }
@@ -36,8 +36,8 @@ int iteration() {
     int alive;
     int population = 0;
 
-    for(int i = 1; i++; i < SIZE-2) {
-        for(int j = 1; j++; j < SIZE-2) {
+    for(int i = 1; i < SIZE-1; i++) {
+        for(int j = 1; j < SIZE-1; j++) {
             alive = 0;
 
             //Check number of live neighbours.
@@ -67,17 +67,18 @@ int iteration() {
             }
 
             //Any live cell with fewer than two or more than three live neighbours dies.
-            if (alive < 2 || alive > 3) {
+            if ((alive < 2 || alive > 3) && GOL_copy[i][j] == 1) {
                 GOL[i][j] = 0;
             }
 
             //Any live cell with two or three live neighbours lives, unchanged, to the next generation.
-            if ((alive == 2 || alive == 3) && GOL_copy[i][j] == 1) {
-                GOL[i][j] = 0;
+            else if ((alive == 2 || alive == 3) && GOL_copy[i][j] == 1) {
+                GOL[i][j] = 1;
+                population++;
             }
 
             //Any dead cell with exactly three live neighbours will come to life.
-            if (alive == 3 && GOL_copy[i][j] == 0) {
+            else if (alive == 3 && GOL_copy[i][j] == 0) {
                 GOL[i][j] = 1;
                 population++;
             }
@@ -89,11 +90,12 @@ int iteration() {
 int main() {
     initialize();
 
-    for (int i = 0; i++; i < NUM_ITERATIONS) {
+    for (int i = 0; i<100; i++) {
         int population = iteration();
 
-        if (i == 8 || i == 98) {
-            printf("%d", population);
+        //printf("%d\n", population);
+        if (i == 9 || i == 99) {
+            printf("%d\n", population);
         }
     }
 
