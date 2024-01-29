15cb19c158f8af4e7cb6618cdf73118bd6f846ce
Lopes30787 <afonsomlopes2001@gmail.com>
2024-01-29 20:50:20 +0100

    FInal Changes to the code

--- GOL.c
+++ GOL.c
@@ -1,4 +1,8 @@
+#include <omp.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "grower.h"
 #include "glider.h"
@@ -12,11 +16,32 @@ int GOL[SIZE][SIZE] = {{0}};
 int GOL_copy[SIZE][SIZE];
 
 //Initialize the current pattern.
-//TODO: Fazer esta merda automatica para nao ter de alterar os nomes das variaveis quando quero testar um pattern diferente.
-void initialize() {
-    for(int i = 0; i < GROWER_HEIGHT; i++) {
-        for(int j = 0; j < GROWER_WIDTH; j++) {
-            GOL[i+1500][j+1500] = grower[i][j];
+void initialize(int pattern) {
+
+    //Initialize grower pattern
+    if (pattern == 1) {
+        for(int i = 0; i < GROWER_HEIGHT; i++) {
+            for(int j = 0; j < GROWER_WIDTH; j++) {
+                GOL[i+1500][j+1500] = grower[i][j];
+            }
+        }
+    }
+
+    //Initialize glider pattern
+    if (pattern == 2) {
+        for(int i = 0; i < GLIDER_HEIGHT; i++) {
+            for(int j = 0; j < GLIDER_WIDTH; j++) {
+                GOL[i+1500][j+1500] = glider[i][j];
+            }
+        }
+    }
+
+    //Initialize beehive pattern
+    if (pattern == 3) {
+        for(int i = 0; i < BEEHIVE_HEIGHT; i++) {
+            for(int j = 0; j < BEEHIVE_WIDTH; j++) {
+                GOL[i+1500][j+1500] = beehive[i][j];
+            }
         }
     }
 }
@@ -31,12 +56,12 @@ void copy_gol() {
     }
 }
 
-//TODO: Paralelizar isto
 void iteration() {
     copy_gol();
     int alive;
     int population = 0;
 
+    //Parallelization of the main cycle
     #pragma omp parallel for private(alive)
     for(int i = 1; i < SIZE-1; i++) {
         for(int j = 1; j < SIZE-1; j++) {
@@ -46,24 +71,31 @@ void iteration() {
             if (GOL_copy[i+1][j] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i+1][j+1] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i+1][j-1] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i][j+1] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i][j-1] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i-1][j+1] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i-1][j] == 1) {
                 alive++;
             }
+
             if (GOL_copy[i-1][j-1] == 1) {
                 alive++;
             }
@@ -86,25 +118,55 @@ void iteration() {
     }
 }
 
-int main() {
-    initialize();
+int main(int argc, char *argv[]) {
+    int pattern;
+    if (argc == 2) {
+        if (strcmp(argv[1], "grower") == 0) {
+            pattern = 1;
+        } 
+        else if (strcmp(argv[1], "glider") == 0) {
+            pattern = 2;
+        } 
+        else if (strcmp(argv[1], "beehive") == 0) {
+            pattern = 3;
+        } 
+        else {
+            printf("Unknown argument.\n");
+            return 0;
+        }
+    }
 
-    for (int i = 0; i<10; i++) {
+    else {
+        printf("Wrong number of arguments.\n");
+        return 0;
+    }
+
+    // Get timing
+    double start,end;
+    start=omp_get_wtime();
+
+    initialize(pattern);
+
+    for (int i = 0; i<NUM_ITERATIONS; i++) {
         iteration();
+    }
 
-        //Population check
-        
-        /*int population = 0;
-        for(int i = 0; i < SIZE; i++) {
-            for(int j = 0; j < SIZE; j++) {
-                if (GOL[i][j] == 1) {
-                    population++;
-                }
+    // Stop timing
+    end=omp_get_wtime();
+
+    // Print result
+    printf("Obtained in %f seconds\n", end-start);
+
+    //Check Final Population
+    int population = 0;
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) {
+            if (GOL[i][j] == 1) {
+                population++;
             }
         }
-        printf("%d\n", population);*/
-        
     }
+    printf("Final Population: %d\n", population);
 
     return 0;
 }
